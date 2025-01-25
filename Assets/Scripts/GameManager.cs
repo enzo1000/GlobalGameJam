5997f8871d8cdd3daca7cbf31d781ad6f949de7a
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-01-25 14:12:40 +0100

    game structure

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,26 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum NewsType
+{
+    NaturalEvent,
+    FollowerBuy,
+    FollowerSell
+}
+
 public class GameManager : MonoBehaviour
 {
     private Bubble bubble;
     private PlayerManager player;
 
-    private float VictoryGoal;
-    // List<Action> visibleActions = new List<Action>();
+    private float victoryGoal;
+    List<ActionScript> visibleActions = new List<ActionScript>();
 
     private float newsTimer;
+    // [TODO] à équilibrer
     private float timeBetweenNews;
     private float newsProba;
+
     void Start()
     {
         bubble = new Bubble();
@@ -21,5 +30,60 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // [TODO] à équilibrer
+    public void UpdateTimeBetweenNews(float nbFollowers)
+    {
+
+    }
+
+    // [TODO] à équilibrer
+    public void UpdateNewProba(float nbFollowers)
+    {
+
+    }
+
+    // --------------|-------------------------------|--------------
+    // bulle explose  bulle n'explose pas et augmente  bulle n'explose pas et descend quand meme (mais peut remonter)
+
+    public void SummonNews(ActionScript action, NewsType type)
+    {
+        float variation = computeVariationValue(action.speculativeBubbleChance);
+
+        switch(type)
+        {
+            case NewsType.NaturalEvent:
+                if (action.asExplosed || Random.value <= action.speculativeBubbleChance) // bulle explose
+                {
+                    variation *= -1.0f;
+                }
+                else
+                {
+                    if (Random.value >= 1 - action.speculativeBubbleChance) // [TODO] à voir
+                    {
+                        variation *= -1.0f;
+                    }
+                }
+                break;
+            case NewsType.FollowerBuy:
+                if (action.asExplosed || Random.value <= action.speculativeBubbleChance) // bulle explose
+                {
+                    variation *= -1.0f;
+                }
+                break;
+            case NewsType.FollowerSell:
+                variation *= -1.0f;
+                break;
+            default:
+                return;
+        }
+    }
+
+    // [TODO] à équilibrer
+    private float computeVariationValue(float speculativeBubbleChance)
+    {
+        float variation = 1f;
+        return variation;
     }
 }
