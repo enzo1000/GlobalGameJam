f190a51398659d281ef42096a728a77a22b5005d
Ame (Adjamé T.R) <adjame.tel.roz@gmail.com>
2025-01-26 00:48:36 +0100

    ajout de la fluctuation

---
+++ Assets/Scripts/VariationActions.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class ActionVariation : MonoBehaviour
+{
+    public float interval = 5f; // Intervalle en secondes pour chaque variation
+    public List<ActionData> actions = new List<ActionData>(); // Liste des actions
+
+    private float timer;
+
+    void Start()
+    {
+        timer = interval;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        // Si le timer atteint 0, générer des variations pour toutes les actions
+        if (timer <= 0f)
+        {
+            GenerateVariations();
+            timer = interval;
+        }
+    }
+
+    void GenerateVariations()
+    {
+        foreach (ActionData action in actions)
+        {
+            float randomValue = Random.Range(0f, 100f); // Générer un nombre aléatoire entre 0 et 100
+
+            if (randomValue <= action.slightFluctuationChance) // Fluctuation légère
+            {
+                float variation = Random.Range(-action.slightFluctuation, action.slightFluctuation);
+                action.currentValue += variation;
+                Debug.Log($"[{action.name}] Fluctuation légère : {variation} -> Nouvelle valeur : {action.currentValue}");
+            }
+            else if (randomValue <= action.slightFluctuationChance + action.crashChance) // Chute/crash
+            {
+                action.currentValue *= action.crashFactor;
+                Debug.Log($"[{action.name}] Chute/crash ! Nouvelle valeur : {action.currentValue}");
+            }
+            else // Montée en flèche
+            {
+                action.currentValue *= action.surgeFactor;
+                Debug.Log($"[{action.name}] Montée en flèche ! Nouvelle valeur : {action.currentValue}");
+            }
+        }
+    }
+}
