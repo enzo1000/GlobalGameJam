e328cc31368bc9aa3d7f7ad0c3cbeab36afb52c5
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-01-25 11:28:41 +0100

    push to rebase

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble
+{
+    public float shellValue { get; set; }
+
+    float delta = 0.25f;
+    float dt = 0.1f;
+
+    public void UpdateValue()
+    {
+        float stdDev = delta * delta * dt;
+        // stdDev *= Random.value * 2;
+        shellValue = shellValue + GaussianRandom(stdDev);
+    }
+
+    public static float GaussianRandom(float stdDev)
+    {
+        float u1 = Random.value;
+        float u2 = Random.value;
+        float randomStdNormal = Mathf.Sqrt(-2f * Mathf.Log(u1, 10)) * Mathf.Sin(2 * Mathf.PI * u2);
+        return randomStdNormal * stdDev;
+    }
+}
