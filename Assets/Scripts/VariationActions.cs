ae5349d586b294df8caf2d5ab45245d2f4f1c3c8
DannyIsWhatToDo <titouan.Vaillant@free.fr>
2025-01-26 17:57:58 +0100

    Koala

--- Assets/Scripts/VariationActions.cs
+++ Assets/Scripts/VariationActions.cs
@@ -91,7 +91,7 @@ public class VariationActions : MonoBehaviour
                 {
                     musicManager.PlayBadVariation(); // Play bad news variation
                 }
-                SetNews("Chute/crash", action.crashFactor);
+                SetNews(action.name, action.crashFactor);
             }
             else // Montée en flèche
             {
@@ -101,7 +101,7 @@ public class VariationActions : MonoBehaviour
                 {
                     musicManager.PlayGoodVariation(); // Play good news variation
                 }
-                SetNews("Montée en flèche", action.surgeFactor);
+                SetNews(action.name, action.surgeFactor);
             }
         }
     }
