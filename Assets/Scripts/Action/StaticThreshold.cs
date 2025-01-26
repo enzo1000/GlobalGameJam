0be80b64a61d760f510e09b66de2b8579da71366
DannyIsWhatToDo <titouan.Vaillant@free.fr>
2025-01-26 17:18:24 +0100

    little change

--- Assets/Scripts/Action/StaticThreshold.cs
+++ Assets/Scripts/Action/StaticThreshold.cs
@@ -6,7 +6,7 @@ public static class StaticThreshold
     public static float lowActionCostThreshold = 75000;
     public static float midActionCostThreshold = 12500;
     public static float highActionCostThreshold = 12501;
-    public static float lowInvestDanger = 3;
-    public static float midInvestDanger = 7;
-    public static float highInvestDanger = 8;
+    public static float lowInvestDanger = 4;
+    public static float midInvestDanger = 8;
+    public static float highInvestDanger = 11;
 }
